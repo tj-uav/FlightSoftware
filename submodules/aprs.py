d171d530cd28f07957a0f680014aa7372cea82aa
Srikar Gouru <srikarg89@gmail.com>
2019-03-26 21:18:10 -0400

    Carbon copy of cubesat

---
+++ submodules/aprs.py
@@ -0,0 +1,157 @@
+import logging
+import os
+import pty
+import time
+from functools import partial
+
+import serial
+
+from core import config
+from helpers.helpers import is_simulate
+from helpers.threadhandler import ThreadHandler
+from . import command_ingest
+from . import eps
+from .command_ingest import command
+
+# Placeholder values for `telemetry.py`
+total_received_ph = 100
+success_checksum_ph = 60
+fail_checksum_ph = 40
+sent_messages_ph = 50
+
+# Initialize global variables
+logger = logging.getLogger("APRS")
+last_telem_time = time.time()
+last_message_time = time.time()
+
+bperiod = 60
+ser = None  # Initialize serial
+
+ser_master, ser_slave = pty.openpty()  # Serial ports for when in simulate mode
+
+
+@command("aprs_echo", str)
+def send(msg: str) -> None:
+    """
+    Put a packet in the APRS queue.  The APRS queue exists
+    only to make sure that we don't send and receive at the
+    same time.
+    :param msg: Message to send into the APRS queue.
+    """
+    global last_message_time
+
+    # Wait until `message_spacing` seconds after the last received message
+    while time.time() - last_message_time < config['aprs']['message_spacing']:
+        time.sleep(1)
+    last_message_time = time.time()
+    ser.write((msg + '\n').encode("utf-8"))  # Send the message
+    time.sleep(1)
+
+
+def telemetry_watchdog():
+    """
+    Watches for "hardware beacon" sent out by APRS. Ensures that the radio is still alive.
+    """
+    while True:
+        time.sleep(config['aprs']['telem_timeout'])
+        if time.time() - last_telem_time > config['aprs']['telem_timeout']:
+            logger.error("APRS is dead, restarting APRS")
+            if not is_simulate('eps'):
+                eps.reboot_device('aprs', 3)
+        else:
+            logger.debug("Watchdog pass APRS")
+
+
+def listen():
+    """
+    Read messages from serial. If a command is received, send it to `command_ingest`
+    """
+    global last_message_time, last_telem_time
+    while True:
+        if is_simulate('aprs'):
+            line = b''
+            while not line.endswith(b'\n'):  # While EOL hasn't been sent
+                res = os.read(ser_master, 1000)
+                line += res
+        else:
+            line = ser.readline()  # Read in a full message from serial
+
+        # Update last message time
+        last_message_time = time.time()
+        if line[0:2] == 'T#':  # Telemetry Packet: APRS special case
+            last_telem_time = time.time()
+            logger.debug('APRS telemetry heartbeat received')
+            continue  # Don't parse telemetry packets
+
+        # Dispatch command
+        parsed = parse_aprs_packet(line)
+        command_ingest.dispatch(parsed)
+
+
+def parse_aprs_packet(packet: str) -> str:
+    """
+    Given a raw radio packet, strip the APRS junk off of it and make it into pure data.
+    :param packet: Input data packet to process.
+    :return: The pure data with all APRS wrappers removed.
+    """
+    raw_packet = str(packet)
+    logger.debug("From APRS: " + raw_packet)
+    header_index = raw_packet.find(':')
+    if header_index == -1:
+        logger.error("Incomplete APRS header!")
+        return ""  # TODO: make sure this is what we want to do.
+    header = raw_packet[:header_index]
+    logger.debug("header: " + header)
+    data = raw_packet[header_index + 1:]
+
+    if len(data) == 0:
+        logger.warning("Empty packet body!")
+        # return ""
+
+    logger.debug("Body: " + data)
+    return data
+    # command_ingest.dispatch(data)
+
+
+def start():
+    global ser
+
+    # Opens the serial port for all methods to use with 19200 baud
+    if is_simulate('aprs'):
+        s_name = os.ttyname(ser_slave)
+        ser = serial.Serial(s_name, 19200)
+        logger.info("Serial started on " + ser.name)
+    else:
+        ser = serial.Serial(config['aprs']['serial_port'], 19200)
+
+    # Create all the background threads
+    t1 = ThreadHandler(target=partial(listen),
+                       name="aprs-listen", parent_logger=logger)
+    # t2 = ThreadHandler(target=partial(send),
+    #                   name="aprs-send_loop", parent_logger=logger)
+    t3 = ThreadHandler(target=partial(telemetry_watchdog),
+                       name="aprs-telemetry_watchdog", parent_logger=logger)
+
+    # Start the background threads
+    t1.start()
+    # t2.start()
+    t3.start()
+
+    # Turn the power on.  TODO: Power check before turn-on.
+    if not is_simulate('eps'):
+        eps.pin_on('aprs')
+
+
+# TODO: Update these methods. Currently only holds placeholder methods.
+def enter_normal_mode():
+    global bperiod
+    bperiod = 60
+
+
+def enter_low_power_mode():
+    global bperiod
+    bperiod = 120
+
+
+def enter_emergency_mode():
+    pass
---
+++ submodules/telemetry.py
@@ -0,0 +1,160 @@
+# TODO: Uses placeholder variables in other files, so make it use actual values
+# TODO: what if radio turned off during a burst?
+import base64
+import collections
+import logging
+import struct
+import time
+from functools import partial
+from threading import Lock
+
+from core import config
+from helpers.threadhandler import ThreadHandler
+from . import adcs
+from . import radio_output
+from .command_ingest import command
+
+logger = logging.getLogger("TELEMETRY")
+
+telem_packet_buffer = collections.deque(
+    maxlen=config['telemetry']['buffer_size'])
+event_packet_buffer = collections.deque(
+    maxlen=config['telemetry']['buffer_size'])
+packetBuffers = [event_packet_buffer, telem_packet_buffer]
+# TODO: Use an indexed system so that we have persistent log storage and querying
+packet_lock = Lock()
+
+
+def telemetry_send():
+    """
+    Thread method to burst the telemetry every so often
+    """
+    # TODO: check battery levels before sending
+    global telem_packet_buffer, event_packet_buffer
+    time.sleep(60)  # Don't send packets straight away
+
+    while True:
+        if (adcs.can_TJ_be_seen() == True and len(telem_packet_buffer) + len(event_packet_buffer) > 0):
+            telemetry_send_once()
+        time.sleep(config['telemetry']['send_interval'])
+
+
+def telemetry_send_once():
+    """
+    Immediately send telemetry packets in both telemetry and event packet queues
+    """
+    global telem_packet_buffer, event_packet_buffer
+    beg_count = len(telem_packet_buffer) + len(event_packet_buffer)
+    send()
+    logger.debug("Sent " + str(beg_count - len(telem_packet_buffer) -
+                               len(event_packet_buffer)) + " telemetry packets")
+
+
+@command("burst")
+def telemetry_burst_command():
+    """
+    Burst command; ignores isTJseen and other checks.
+    """
+    send(ignoreADCS=True)
+
+
+def enqueue_event_message(event):
+    """
+    Enqueue an event message.
+    :param event: message to enqueue. Maximum 16 bytes. If over then throws exception, if under then pads with '='
+    """
+
+    if len(event.encode('utf-8')) > 16:
+        logger.error("Event message larger than 16 bytes, message is " +
+                     str(len(event.encode('utf-8'))) + " bytes long")
+        return
+    elif len(event.encode('utf-8')) != 16:
+        # logger.error("Event message must be exactly 16 bytes, message is " +
+        #             str(len(event.encode('utf-8'))) + " bytes long")
+        # return
+        while (len(event.encode('utf-8')) != 16):
+            event += "="
+
+    global event_packet_buffer, packet_lock
+    with packet_lock:
+        packet = "Z"
+        packet += str(base64.b64encode(struct.pack('d', time.time())))
+        packet += str(base64.b64encode(event))
+        event_packet_buffer.append(packet)
+
+
+def enqueue_submodule_packet(packet):
+    """
+    Accepts a single raw string packet and enqueues them into the telemetry packet buffer.
+    :param packet: A correctly formatted string subpacket to enqueue.
+    """
+    global telem_packet_buffer, packet_lock
+    with packet_lock:
+        telem_packet_buffer.append(packet)
+
+
+def enqueue_submodule_packets(packets):
+    """
+    Accepts raw string packets and enqueues them into the telemetry packet buffer.
+    :param packets: A list of string subpackets to enqueue. These packets must be formatted correctly
+    """
+    global telem_packet_buffer, packet_lock
+    with packet_lock:
+        for packet in packets:
+            telem_packet_buffer.append(packet)
+
+
+def send(ignoreADCS=False, radio='aprs'):
+    """
+    Concatenates packets to fit in max_packet_size (defined in config) and send through the APRS, dequing the packets in the process
+    :param ignoreADCS: If true, ignores ADCS canTJBeSeen.
+    :param radio: Radio to send telemetry over, either "aprs" or "iridium"
+    """
+    global packetBuffers, event_packet_buffer, telem_packet_buffer, packet_lock
+    squishedPackets = ""
+
+    with packet_lock:
+        # packet_lock.acquire()
+        while len(event_packet_buffer) + len(telem_packet_buffer) > 0 and (adcs.can_TJ_be_seen() or ignoreADCS):
+            for buffer in packetBuffers:
+                while len(buffer) > 0 and len(squishedPackets) < config['telemetry']['max_packet_size'] and (
+                        adcs.can_TJ_be_seen() or ignoreADCS):
+                    # test = buffer.pop()
+                    # print(test)
+                    # squishedPackets += test
+                    squishedPackets += buffer.pop()
+
+            # TODO: alternate between radios
+            logger.debug(squishedPackets)
+            radio_output.send(squishedPackets, radio)
+            squishedPackets = ""
+            time.sleep(6)
+
+    # packet_lock.release()
+
+
+@command("clear")
+def clear_buffers():
+    global packet_lock, event_packet_buffer, telem_packet_buffer
+    with packet_lock:
+        event_packet_buffer.clear()
+        telem_packet_buffer.clear()
+
+
+def start():
+    """
+    Starts the telemetry send thread
+    """
+
+    t2 = ThreadHandler(target=partial(telemetry_send),
+                       name="telemetry-telemetry_send")
+    t2.start()
+
+
+# TODO: Need to know what needs to be done in low power and emergency modes.
+def enter_emergency_mode():
+    pass  # TODO: change config
+
+
+def enter_low_power_mode():
+    pass
