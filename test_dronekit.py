23459aa9cf3ce7345ee05edf578b54507bcbc0c5
gnanduru1 <ganesh.nanduru@gmail.com>
2020-11-14 09:23:26 -0500

    first dronekit test

--- test_dronekit.py
+++ test_dronekit.py
@@ -1,22 +1,16 @@
 from dronekit import connect, VehicleMode
-import cv2
+
 import time
 import os
 
-connection_string = '/dev/ttyACM0'
-baudrate = 921600
+connection_string = '/dev/ttyUSB0'
+baudrate = 115200
 
-try:
-    # Connect to the Vehicle.
-    vehicle = connect(connection_string, wait_ready=True, baud=baudrate)
-    gps = vehicle.gps_0
-    if gps.fix_type < 2:
-        print("No GPS Fix\n")
-    print("Lat, lon, alt, yaw")
-    print(loc.lat, loc.lon, loc.alt, attitude)       
-except:
-    out = open("error.txt", "w+")
-    out.write("ERROR REEEE: " + str(time.time()))
-    out.close()
-finally:
-    vehicle.close()
+# Connect to the Vehicle.
+vehicle = connect(connection_string, wait_ready=True, baud=baudrate)
+gps = vehicle.gps_0
+if gps.fix_type < 2:
+	print("No GPS Fix\n")
+print("Lat, lon, alt, yaw")
+print(loc.lat, loc.lon, loc.alt, attitude)
+vehicle.close()
