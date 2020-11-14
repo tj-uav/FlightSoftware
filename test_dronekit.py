92e9705b8a9005be42428cd8ac241470ace49a23
gnanduru1 <ganesh.nanduru@gmail.com>
2020-11-14 09:48:09 -0500

    dronekit test 2

--- test_dronekit.py
+++ test_dronekit.py
@@ -3,14 +3,16 @@ from dronekit import connect, VehicleMode
 import time
 import os
 
-connection_string = '/dev/ttyUSB0'
+connection_string = '/dev/ttyACM0'
 baudrate = 115200
 
 # Connect to the Vehicle.
 vehicle = connect(connection_string, wait_ready=True, baud=baudrate)
+loc = vehicle.location.global_frame
 gps = vehicle.gps_0
+attitude = vehicle.attitude
 if gps.fix_type < 2:
 	print("No GPS Fix\n")
-print("Lat, lon, alt, yaw")
-print(loc.lat, loc.lon, loc.alt, attitude)
+print(loc)
+print(attitude)
 vehicle.close()
