1a270155fbbac24340d9ccbd6f08fd87174c14b2
Krishnan Shankar <krishnans2006@gmail.com>
2022-01-16 19:58:46 -0500

    Migrate to SocketIO

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
 import os
-import socket
+import socketio
 import sys
-from time import sleep
+import requests
 
 from dotenv import load_dotenv
 
@@ -18,33 +18,40 @@ FlightSoftware requires a connection to GroundStation in order to send images. \
 Make sure GroundStation is running and is accepting connections. 
 Clone GroundStation using any of the following commands:
 
-    git clone -b dev git@github.com:tj-uav/GroundStation.git
-    git clone -b dev https://github.com/tj-uav/GroundStation.git
+    git clone git@github.com:tj-uav/GroundStation.git
+    git clone https://github.com/tj-uav/GroundStation.git
 
 Make sure to run the 'app.py' file to start the socket connection.
 --------------------
 """)
 
-while True:  # Attempts to re-initiate connection on connection loss
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((config["conn"]["host"], config["conn"]["port"]))
-            print("[SUCCESS] Connected to GroundStation")
-            img = os.getenv("IMAGE")  # Any sample image in base64 format
-            # Spaces mark the end of the message, and therefore cannot be included in the base64 string
-            # base64 strings ignore spaces, which allows space delimiters to work
-            img = img.replace(" ", "")
-            send = bytes(img, "utf-8") + b" "
-            s.sendall(send)
-            while True:
-                resp = s.recv(4096)
-                if not resp:
-                    print("""[ ERROR ] Connection to GroundStation was closed, possibly due to program termination. FlightSoftware is attempting to re-initiate a connection.""")
-                    break
-                resp = resp.decode("utf-8")
-                if resp == "Image Complete":  # Once image has been completely sent
-                    sleep(2)  # Wait two seconds to simulate delay from camera
-                    s.sendall(send)  # Send the same image again as a new image
-    except ConnectionRefusedError as e:
-        print("[ ERROR ] Connection Refused: Make sure GroundStation is running and accepting connections")
-    sleep(5)
+sio = socketio.Client()
+
+
+def send_image_loop():
+    while True:
+        img = os.getenv("IMAGE")
+        try:
+            sio.emit("image", {"image": img})
+        except:
+            pass
+        sio.sleep(2)
+
+
+@sio.event
+def connect():
+    print("[SUCCESS] Connected to GroundStation with sid:", sio.sid)
+    sio.start_background_task(target=send_image_loop)
+
+
+@sio.event
+def connect_error(_):
+    print("[ ERROR ] Connection Refused: Make sure GroundStation is running and accepting connections.")
+
+
+@sio.event
+def disconnect():
+    print("[ ERROR ] Connection to GroundStation was closed, possibly due to program termination. FlightSoftware is attempting to re-initiate a connection.")
+
+
+sio.connect("http://localhost:4000")
