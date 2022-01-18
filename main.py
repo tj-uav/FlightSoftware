5cd64be7b3976f43ece6dd4b186429d65ee79552
Krishnan Shankar <krishnans2006@gmail.com>
2022-01-17 21:38:52 -0500

    Add gimbal support + dummy functions for everything

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import base64
 import json
 import os
-import socketio
 import sys
+import time
+from threading import Thread
+
 import cv2 as cv
-
+import gpiozero
+import socketio
 from dotenv import load_dotenv
-
-sys.tracebacklimit = 0
-
-load_dotenv()
-
-with open("config.json", "r") as file:
-    config = json.load(file)
 
 print("""--------------------
 FlightSoftware requires a connection to GroundStation in order to send images. \
@@ -26,67 +22,114 @@ Make sure to run the 'app.py' file to start the socket connection.
 --------------------
 """)
 
-sio = socketio.Client()
+sys.tracebacklimit = 0
+
+load_dotenv()
+
+with open("config.json", "r") as file:
+    config = json.load(file)
 
 WIDTH = config["image"]["width"]
 HEIGHT = config["image"]["height"]
 
+img_counter = 0
+img_send_counter = 0
 
-def dummy_send_images():
-    while True:
-        img = os.getenv("IMAGE")
-        try:
-            sio.emit("image", {"image": img})
-        except:
-            pass
-        sio.sleep(2)
+existing_files = len([name for name in os.listdir("images") if os.path.isfile(os.path.join("images", name)) and name.endswith(".png")])
+if existing_files > 1:
+    img_counter = existing_files - 1
+    img_send_counter = existing_files - 1
+
+button = None if config["gimbal"]["dummy"] else gpiozero.Button(config["gimbal"]["pin"], pull_up=False)
 
 
-def send_images():
+def take_images(sock):
+    global img_counter
     cam = cv.VideoCapture(config["image"]["device"])  # , cv.CAP_DSHOW)
     cam.set(3, WIDTH)
     cam.set(4, HEIGHT)
-    img_counter = 0
-    img_send_counter = 0
     while True:
-        sio.sleep(2)
+        sock.sleep(0.5)
+        if not config["gimbal"]["dummy"] and not button.is_pressed:  # If the gimbal exists and is not ready to take pictures
+            print("[ INFO  ] Waiting for confirmation from gimbal")
+            continue  # Wait longer
         ret, frame = cam.read()
         if not ret:
             print("[ ERROR ] Failed to grab frame from camera")
             continue
+        print(f"[ INFO  ] {img_counter} saving...")
+        cv.imwrite(f"images/image_{img_counter}_100.jpg", frame, [int(cv.IMWRITE_JPEG_QUALITY), 100])
+        cv.imwrite(f"images/image_{img_counter}_095.jpg", frame, [int(cv.IMWRITE_JPEG_QUALITY), 95])
+        cv.imwrite(f"images/image_{img_counter}_090.jpg", frame, [int(cv.IMWRITE_JPEG_QUALITY), 90])
+        cv.imwrite(f"images/image_{img_counter}_075.jpg", frame, [int(cv.IMWRITE_JPEG_QUALITY), 75])
+        cv.imwrite(f"images/image_{img_counter}_050.jpg", frame, [int(cv.IMWRITE_JPEG_QUALITY), 50])
+        cv.imwrite(f"images/image_{img_counter}.bmp", frame)
+        cv.imwrite(f"images/image_{img_counter}.png", frame)
+        print(f"[ INFO  ] {img_counter} saved.")
+        img_counter += 1
+
+
+def dummy_take_images(sock):
+    global img_counter
+    img = os.getenv("IMAGE")
+    while True:
+        sock.sleep(1.1)  # Account for delay in saving images
+        with open(f"images/image_{img_counter}.png", "wb") as image_file:
+            image_file.write(base64.b64decode(img))
+        print(f"[ INFO  ] {img_counter} saved.")
+        img_counter += 1
+
+
+def send_images(sock):
+    global img_send_counter
+    while True:
+        sock.sleep(1)  # Delay for one second before sending images
         try:
-            ret, buffer = cv.imencode(".png", frame)
-            b64 = base64.b64encode(buffer)
-            sio.emit("image", {"image": b64})
-            while img_send_counter < img_counter:
-                with open(f"images/image_{img_send_counter}.jpg", "rb") as img_file:
-                    encoded = base64.b64encode(img_file.read())
-                    sio.emit("image", {"image": encoded})
-                print(f"[ INFO  ] Image file \"image_{img_send_counter}.jpg\" sent to GroundStation")
+            if img_send_counter < img_counter:
+                with open(f"images/image_{img_send_counter}.png", "rb") as image_file:
+                    img = base64.b64encode(image_file.read())
+                    sock.emit("image", {"image": img})
+                print(f"[ INFO  ] {img_send_counter} sent!")
                 img_send_counter += 1
         except:
-            cv.imwrite(f"images/image_{img_counter}.jpg", frame)
-            print(f"[ INFO  ] Image written to file: \"image_{img_counter}.jpg\"")
-            img_counter += 1
-    cam.release()
+            pass
 
 
-@sio.event
-def connect():
-    print("[SUCCESS] Connected to GroundStation with sid:", sio.sid)
+def dummy_send_images():
+    global img_send_counter
+    while True:
+        time.sleep(1)
+        if img_send_counter < img_counter:
+            print(f"[ INFO  ] {img_send_counter} sent!")
+            img_send_counter += 1
 
 
-@sio.event
-def connect_error(_):
-    print("[ ERROR ] Connection Refused: Make sure GroundStation is running and accepting connections.")
+if config["groundstation"]["dummy"]:
+    take = dummy_take_images if config["image"]["dummy"] else take_images
+    take_t = Thread(target=take, args=(time,))
+    send_t = Thread(target=dummy_send_images)
+    take_t.start()
+    send_t.start()
+else:
+    sio = socketio.Client()
+
+    @sio.event
+    def connect():
+        print("[SUCCESS] Connected to GroundStation with sid:", sio.sid)
 
 
-@sio.event
-def disconnect():
-    print("[ ERROR ] Connection to GroundStation was closed, possibly due to program termination. FlightSoftware is attempting to re-initiate a connection.")
+    @sio.event
+    def connect_error(_):
+        print("[ ERROR ] Connection Refused: Make sure GroundStation is running and accepting connections.")
 
 
-sio.connect(f"http://{config['groundstation']['host']}:{config['groundstation']['port']}")
+    @sio.event
+    def disconnect():
+        print("[ ERROR ] Connection to GroundStation was closed, possibly due to program termination. FlightSoftware is attempting to re-initiate a connection.")
 
-target = dummy_send_images if config["image"]["dummy"] else send_images
-sio.start_background_task(target=target)
+
+    sio.connect(f"http://{config['groundstation']['host']}:{config['groundstation']['port']}")
+
+    take = dummy_take_images if config["image"]["dummy"] else take_images
+    sio.start_background_task(take, sio)
+    sio.start_background_task(send_images, sio)
