5846d676b7782b74c0af4177a6f8d95e4552c6ff
Krishnan Shankar <krishnans2006@gmail.com>
2022-11-03 18:22:50 -0400

    Activate virtual environment when starting service

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ mv /home/tjuav/FlightSoftware/assets/images/sample.png /home/tjuav/FlightSoftwar
 mv /home/tjuav/FlightSoftware/assets/images "/home/tjuav/FlightSoftware/$(date +%F-%T)"
 mkdir /home/tjuav/FlightSoftware/assets/images
 mv /home/tjuav/FlightSoftware/assets/sample.png /home/tjuav/FlightSoftware/assets/images/sample.png
+source /home/tjuav/FlightSoftware/venv/bin/activate
 python /home/tjuav/FlightSoftware/main.py
